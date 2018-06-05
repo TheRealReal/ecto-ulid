b542360f71591ed5fea4927a3ca91617ebf814de
David Cuddeback <david.cuddeback@gmail.com>
2018-06-05 11:07:06 -0600

    Initial commit

---
+++ lib/ecto_ulid.ex
@@ -0,0 +1,18 @@
+defmodule Ecto.ULID do
+  @moduledoc """
+  Documentation for Ecto.ULID.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ecto.ULID.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_ulid_test.exs
@@ -0,0 +1,8 @@
+defmodule Ecto.ULIDTest do
+  use ExUnit.Case
+  doctest Ecto.ULID
+
+  test "greets the world" do
+    assert Ecto.ULID.hello() == :world
+  end
+end
