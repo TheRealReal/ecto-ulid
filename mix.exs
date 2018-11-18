cf5c4cc0b933856dc9c51b2e1b9e3fa3ef1419c9
Xavier Demompion <xwar_srkn@hotmail.com>
2018-11-18 05:58:32 -0800

    Accept ecto 2.* and 3.* instead of 2.* only.

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Ecto.ULID.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 2.0"},
+      {:ecto, "~> 2.0 or ~> 3.0"},
       {:benchfella, "~> 0.3.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
     ]
