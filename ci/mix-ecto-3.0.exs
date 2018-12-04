4c3bf888fe34e6a1807fc9836643374fed042940
David Cuddeback <david.cuddeback@gmail.com>
2018-12-03 18:45:45 -0700

    Add Elixir 1.7 and Ecto 3.0 to CI matrix

---
+++ ci/mix-ecto-3.0.exs
@@ -0,0 +1,27 @@
+defmodule Ecto.ULID.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ecto_ulid,
+      version: "0.1.0",
+      elixir: "~> 1.2",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:ecto, "~> 3.0.0"},
+      {:benchfella, "~> 0.3.5", only: [:dev, :test]},
+    ]
+  end
+end
+
