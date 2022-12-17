03dbbb8a6447dbca442cf656e4fdff18a289be86
Darwin D Wu <darwin67@users.noreply.github.com>
2022-12-16 17:24:11 -0800

    [CI]: Add CI setup using Github actions

---
+++ .formatter.exs
@@ -0,0 +1,7 @@
+[
+  inputs: [
+    "*.{ex,exs}",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "credo/**/*.{ex,exs}"
+  ]
+]
--- mix.exs
+++ mix.exs
@@ -6,14 +6,14 @@ defmodule Ecto.ULID.Mixfile do
       app: :ecto_ulid,
       version: "0.3.0",
       elixir: "~> 1.4",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "Ecto.ULID",
       description: "An Ecto.Type implementation of ULID.",
       package: package(),
-      source_url: "https://github.com/TheRealReal/ecto-ulid",
-      homepage_url: "https://github.com/TheRealReal/ecto-ulid",
-      docs: [main: "Ecto.ULID"],
+      source_url: "https://github.com/oviceinc/ecto-ulid",
+      homepage_url: "https://github.com/oviceinc/ecto-ulid",
+      docs: [main: "Ecto.ULID"]
     ]
   end
 
@@ -25,9 +25,9 @@ defmodule Ecto.ULID.Mixfile do
 
   defp package do
     [
-      maintainers: ["David Cuddeback"],
+      maintainers: ["oVice Developers"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/TheRealReal/ecto-ulid"},
+      links: %{"GitHub" => "https://github.com/oviceinc/ecto-ulid"}
     ]
   end
 
@@ -36,6 +36,15 @@ defmodule Ecto.ULID.Mixfile do
       {:ecto, "~> 2.0 or ~> 3.0"},
       {:benchfella, "~> 0.3.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      # linter
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      fmt:
+        "format --check-formatted mix.exs 'lib/**/*.{ex,exs}' 'test/**/*.{ex,exs}' 'priv/**/*.{ex,exs}' 'config/*.{ex,exs}'"
     ]
   end
 end
